a59d8cb887703935e9f21a005a493cc24aeb944f
OMMICANG <ommicang@gmail.com>
2025-06-28 19:46:42 -0700

    <UPD: Minor Padding Changes>

--- devcheckinn/components/Header.tsx
+++ devcheckinn/components/Header.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { BiSolidPhoneCall } from "react-icons/bi";
 import { MdLocationPin } from "react-icons/md";
-import MenuIcon from './MenuIcon';  
+import MenuIcon from './MenuIcon';
+// import { Button } from '@/components/ui/button';  
 import styles from '../styles/Header.module.css';
 
 export default function Header() {
@@ -11,18 +13,40 @@ export default function Header() {
     <header>
         <div className={styles.headerContainer}>
 
-            <div className={styles.headerContent}>
+            <div className={`${styles.headerContent} ${styles.dropIn}`}>
 
                 <div className={styles.logoWrapper}>
-                    <Image priority={true} src="/logo/Logo_-_Copy-removebg-preview.png" alt="Check Inn Logo" width={20} height={20} className={styles.logo} />
+                    <Image priority={true} 
+                    src="/logo/Logo_-_Copy-removebg-preview.png" 
+                    alt="Check Inn Logo" width={25} height={25} 
+                    className={styles.logo} />
                 </div>
 
                 <div className={styles.topContainer}>
-                    <div className={styles.mobile}><BiSolidPhoneCall className={styles.callIcon} />+2348030555150</div>
-                    <a href="https://tinyurl.com/vwdv3pft" target="_blank" rel="noopener noreferrer" className={styles.topLocator}><MdLocationPin className={styles.locationIcon}/>ojodu . Berger</a>
+
+                    <div className={styles.mobile}>
+                    <BiSolidPhoneCall className={styles.callIcon} />
+                    <span>+2348030555150</span> 
+                    </div>
+
+                    <a 
+                    href="https://tinyurl.com/vwdv3pft" 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    className={styles.topLocator}
+                    >
+
+                    <MdLocationPin 
+                    className={styles.locationIcon}/>
+                    <span> ojodu . Berger</span>
+
+                    </a>
                 </div>
 
+                <Link href="/rooms">
                 <button className={styles.button}>Book a room</button>
+                </Link>
+
                 <div className={styles.menuIcon}><MenuIcon /></div>
 
             </div>
